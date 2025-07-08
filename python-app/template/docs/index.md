2bf328079edca81331ed5d68102c1cbf7e926292
Kobi Begas <kobib@yaelsoft.com>
2025-07-08 14:37:38 +0300

    Add python-app template

---
+++ python-app/template/docs/index.md
@@ -0,0 +1,11 @@
+# Documents for ${{values.app_name}}
+
+this application use two endpoints:
+- /api/v1/info
+- /api/v1/health
+
+# How to access the app?
+you can access the app by trying this url:
+- http://${{values.app_name}}-${{values.app_env}}.test.com/api/v1/info
+
+
---
+++ python-app/template/src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import datetime, socket
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "hostname":  socket.gethostname(),
+        "message": "cicd",
+        "deployed_on": "k8s",
+        "env": "${{values.app_env}}",
+        "app_name": "${{values.app_name}}"
+    })
+
+@app.route('/api/v1/health')
+
+def health():
+    return jsonify({
+        "status": "up"
+    }), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
